0a41f20c8a6baaefdaf52f6c853450400553bbd0
Eldorbek <Eldorbekmakhkamov@gmail.com>
2020-03-21 10:51:47 +0500

    Styles changed

--- storage/cms/cache/6b/2f/nav.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5e74e7c6e4e1a323517773_d56ff8eab36bfec1aa51b902dbfc0fd4Class extends Cms\Classes\PartialCode
-{
-
-}
--- storage/cms/cache/8c/47/comment.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5e74ebc50d84e468275793_ca04d4228e629401f708a603d4b779dfClass extends Cms\Classes\PartialCode
-{
-
-}
